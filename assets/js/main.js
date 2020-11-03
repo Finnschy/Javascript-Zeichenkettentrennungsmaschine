9bb52102477196bbe4b6f25cd819d395646f9a3b
Finnschy <skylerfearless@yahoo.de>
2020-11-03 15:47:57 +0100

    fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,26 @@
 // zeichenkettentrennungsmaschine 
 
-let toCutString = document.getElementById("toCutString")
-let posString = document.getElementById("posString")
-let beforeCut = document.getElementById("beforeCut")
-let afterCut = document.getElementById("afterCut")
 
-const cutString = () => {
+function cutString() {
     event.preventDefault()
-    if (beforeCut.checked) {
-        console.log("Davor");
-        
-    } else if (afterCut.checked) {
-        console.log("Danach");
-    } 
+    let toCutString = document.getElementById("toCutString");
+    let posString = document.getElementById("posString");
+    let beforeCut = document.getElementById("beforeCut");
+    let beforeOutput = document.getElementById("beforeOutput");
+    let afterOutput = document.getElementById("afterOutput");
+    let stringSplit = toCutString.value.split(posString.value)
+    let positionStringLength = posString.value.length;
+
+
+    if ((toCutString.value.indexOf(posString.value) == -1) || positionStringLength <= 0){
+        beforeOutput.innerHTML = "Das Zeichen konnte leider nicht gefunden werden"
+        afterOutput.innerHTML = toCutString.value
+    } else if (beforeCut.checked) {
+        beforeOutput.innerHTML = stringSplit[0] + posString.value;
+        afterOutput.innerHTML = stringSplit[1]
+    } else {
+        afterOutput.innerHTML = posString.value + stringSplit[1];
+        beforeOutput.innerHTML = stringSplit[0]
+    }
 }
+
