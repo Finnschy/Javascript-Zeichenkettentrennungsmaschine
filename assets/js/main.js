13f5113df08590c6401d09d4d47b8fa23a287804
Finnschy <skylerfearless@yahoo.de>
2020-11-03 12:53:57 +0100

    ein paar html und css fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 // zeichenkettentrennungsmaschine 
+
+let toCutString = document.getElementById("toCutString")
+let posString = document.getElementById("posString")
+let beforeCut = document.getElementById("beforeCut")
+let afterCut = document.getElementById("afterCut")
+
+const cutString = () => {
+    event.preventDefault()
+    if (beforeCut.checked) {
+        console.log("Davor");
+        
+    } else if (afterCut.checked) {
+        console.log("Danach");
+    } 
+}
